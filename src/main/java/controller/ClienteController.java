4eec0c09502bc56c32a6cfe827f2ed507f0cce8e
Juan-Analitcs-Data <hidanakt73@gmail.com>
2026-07-13 20:44:36 -0300

    feat: Criando Classe ClienteController, e inserindo os metodos de salvar cliente e validar as informações

---
+++ src/main/java/controller/ClienteController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import dao.ClienteDAO;
+import model.Cliente;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class ClienteController {
+
+    private final ClienteDAO clienteDAO = new ClienteDAO();
+
+    public void salvar(Cliente cliente) throws Exception {
+        validar(cliente);
+
+        try {
+            if(cliente.getIdCliente() == 0){
+                clienteDAO.inserir(cliente);
+            }else{
+                clienteDAO.atualizar(cliente);
+            }
+        } catch (Exception e) {
+            if(e.getCause() instanceof SQLException){
+                SQLException sqlEx = (SQLException) e.getCause();
+                if("23505".equals(sqlEx.getSQLState())){
+                    throw new Exception("Já existe um cliente cadastrado com esse CPF.");
+                }
+            }
+            throw new Exception("Erro ao salvar cliente: " + e.getMessage());
+        }
+    }
+
+    private void validar(Cliente cliente) throws Exception {
+        if (cliente.getNome() == null || cliente.getNome().trim().isEmpty()) {
+            throw new Exception("O nome do cliente é obrigatório.");
+        }
+        if (cliente.getTelefone() == null || cliente.getTelefone().trim().isEmpty()) {
+            throw new Exception("O telefone do cliente é obrigatório.");
+        }
+        if (cliente.getCpf() == null || !cliente.getCpf().matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")) {
+            throw new Exception("CPF inválido. Use o formato 000.000.000-00.");
+        }
+    }
+}
